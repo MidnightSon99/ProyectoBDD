31cc2bb5319e4e542749ad98dee06d5752d0d2bf
IsaacMora99 <23151230@gmail.com>
2026-04-15 14:10:49 -0600

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,21 @@
+namespace ProyectoBDD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FormGrafo f = new FormGrafo();
+            f.Show();
+        }
+    }
+}
